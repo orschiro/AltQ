711ac1df8bc200af28157e06705146621317aa92
Robert Orzanna <orschiro@gmail.com>
2013-07-08 11:39:26 +0200

    1.1 released
    Switch from JS keyboard implementation through addHotkey.js to native
    Chrome keyboard commands, chrome.commands. See
    http://developer.chrome.com/extensions/commands.html

--- background.js
+++ background.js
@@ -1,37 +1,36 @@
 var previousTab;
 var currentTab;
 
-function init() {
-	chrome.tabs.getSelected(null, function(tab) {
-	previousTab = tab.id;
-	currentTab = null;
-	});
-}
-
-// Toggle if user clicks on the extension icon
+// General functions
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.update(previousTab, {selected: true});
+    chrome.tabs.update(previousTab, {selected: true});
 });
 
-// Toggle if user presses the keyboard shortcut
+chrome.tabs.onSelectionChanged.addListener(function(tab) {
+    if (previousTab == null) {
+        previousTab = tab;
+    }
+    if (currentTab == null) {
+        currentTab = tab;
+    }
+    else {
+        previousTab = currentTab;
+        currentTab = tab;
+    }
+});
+
+
+// Keyboard shortcut toggle function
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "toggle") {
 
-	init();
+    chrome.tabs.getSelected(null, function(tab) {
+        previousTab = tab.id;
+        currentTab = null;
+    });
 
-	chrome.tabs.onSelectionChanged.addListener(function(tab) {
-		if (previousTab == null) {
-			previousTab = tab;
-		}
-		if (currentTab == null) {
-			currentTab = tab;
-		}
-		else {
-			previousTab = currentTab;
-			currentTab = tab;
-		}
-	});
-
-		chrome.tabs.update(previousTab, {selected: true});
-	}
+    chrome.tabs.update(previousTab, {selected: true});
+  }
 });
+
+
