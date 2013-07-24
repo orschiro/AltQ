9676bf38d0c9a810f652eaf7eee7eb2892d09371
Robert Orzanna <orschiro@gmail.com>
2013-07-24 16:32:50 +0200

    1.2 released
    - Switched from onSelectionChanged to onActivated: When opening a new
    tab, switch to last focused tab is now possible.
    
    - Use local storage and persistent: true: Fixes issue with Chrome not
    saving the tab variables persistently

--- background.js
+++ background.js
@@ -6,16 +6,37 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.update(previousTab, {selected: true});
 });
 
-chrome.tabs.onSelectionChanged.addListener(function(tab) {
+chrome.tabs.onActivated.addListener(function(tab) {
     if (previousTab == null) {
         previousTab = tab;
+        // Save it using the Chrome extension storage API.
+		chrome.storage.sync.set({'value': previousTab}, function() {
+            // Notify that we saved.
+			message('Settings saved');
+        });
+		
     }
     if (currentTab == null) {
         currentTab = tab;
+        // Save it using the Chrome extension storage API.
+		chrome.storage.sync.set({'value': currentTab}, function() {
+            // Notify that we saved.
+			message('Settings saved');
+        });
     }
     else {
         previousTab = currentTab;
+        // Save it using the Chrome extension storage API.
+		chrome.storage.sync.set({'value': previousTab}, function() {
+            // Notify that we saved.
+			message('Settings saved');
+        });
         currentTab = tab;
+        // Save it using the Chrome extension storage API.
+		chrome.storage.sync.set({'value': currentTab}, function() {
+            // Notify that we saved.
+			message('Settings saved');
+        });
     }
 });
 
